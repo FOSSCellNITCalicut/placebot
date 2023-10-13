2858aa14caa2218a9b8054e5b8179874b358eacc
Madhav Prabhu <dpmadhav@hotmail.com>
2023-10-13 11:22:06 +0530

    Updated Test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 PR_REPO_URL=$1
 BRANCH=$2
@@ -6,14 +6,14 @@ BRANCH=$2
 rm -rf testrepo
 git clone $PR_REPO_URL -b $BRANCH testrepo >/dev/null
 
-node check.js place.fosscell.org/test
+node check.js place.fosscell.org/test > /dev/null
 ret=$?
 if [ $ret -ne 0 ]; then
 	echo -ne -1
 	exit
 fi
 
-node getimg.js place.fosscell.org orig.png
-node getimg.js place.fosscell.org/test new.png
+node getimg.js place.fosscell.org orig.png > /dev/null
+node getimg.js place.fosscell.org/test new.png > /dev/null
 
 echo -n $(compare -metric AE orig.png new.png /tmp/temp.png 2>&1 >/dev/null)
